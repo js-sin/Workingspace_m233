9b4ebfcb80cf83b4b202ef5617c856693022f54c
Jaspreet <jaspreet.singh@lernende.bbw.ch>
2022-09-28 13:58:01 +0000

    created tests

---
+++ src/test/java/OneRescourceTest.java
@@ -0,0 +1,43 @@
+package ch.zli.m223;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+
+public class OneRescourceTest{
+    //@
+     @Test
+    public void userEndpoint401() {
+        given()
+          .when().get("/user")
+          .then()
+            .statusCode(401);
+    }
+     @Test
+    public void bookingEndpoint401() {
+        given()
+          .when().get("/booking")
+          .then()
+            .statusCode(401);
+    }
+    @Test
+    public void bookingplaceEndpoint200(){
+         given()
+          .when().get("/bookingplace")
+          .then()
+             .statusCode(200)
+            .body(is("[]"));
+    }
+   
+    @Test
+    public void rolesEndpoint401() {
+        given()
+          .when().get("/roles")
+          .then()
+            .statusCode(401);
+    }
+   
+}
