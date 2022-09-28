8ab00104090f57445bab0cc76d3e0448628e2a5a
Jaspreet <jaspreet.singh@lernende.bbw.ch>
2022-09-28 06:46:17 +0000

    created Controller

--- src/main/java/ch/zli/m223/model/User.java
+++ src/main/java/ch/zli/m223/model/User.java
@@ -1,5 +1,71 @@
 package ch.zli.m223.model;
 
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+
+import ch.zli.m223.model.User;
+import ch.zli.m223.service.UserService;
+
+
+@Path("/user")
+@Tag(name = "user", description = "Handling of User")
+public class UserController {
+
+    @Inject
+    UserService userService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Index all Users.", description = "Returns a list of all users.")
+    public List<User> indexById() {
+        return userService.findAll();
+    }
+
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Index all Users.", description = "Returns a list of all users.")
+    @Path("/{id}")
+    public List<User> index(Long id) {
+        return List.of(userService.getUserById(id));
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(summary = "Creates a new user.", description = "Creates a new user and returns the newly added user.")
+    public User create(User user) {
+       return userService.createUser(user);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void delete(long id) {
+    userService.deleteUser(id);
+    }
+
+     @PUT
+     @Path("/{id}")
+      public void update(Long id, User user) {
+        user.setId(id);
+        userService.update(user);
+     }
+
+
+/* 
 @Enitity 
 public class User{
     @Id
@@ -110,6 +176,6 @@ public class User{
 		this.password = password;
 	}
 
-
+*/
 
 }
